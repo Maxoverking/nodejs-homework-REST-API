eb73a11cf065c687de9f3192d2aeed268a6d374c
Maxoverking <avemax7@gmail.com>
2023-03-17 19:14:43 +0100

    перенес миддлварс

--- routes/api/router.js
+++ routes/api/router.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const {
-  checkUser,
-  validUser,
-} = require("../../controllers/middleware/middleware");
 
 const {
   getAllUser,
@@ -13,6 +9,8 @@ const {
   deleteUser,
   updateStatusContact,
 } = require("../../controllers/controllers");
+const { checkUser } = require("../../middlewares/checkUser");
+const { validUser } = require("../../middlewares/validUser");
 
 router.get("/", getAllUser);
 
